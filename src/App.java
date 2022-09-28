56817e27d849a4bd04dd199916dd429c6398e58a
guhans6 <guhans61101@gmail.com>
2022-09-28 14:00:36 +0530

    AccessModifiers changed

--- src/App.java
+++ src/App.java
@@ -1,14 +1,13 @@
+import java.util.Stack;
 
-
-public class App {
+class App {
     
     //Movie guesser game that reads movies from file like fill in the blanks
    
     public static void main(String args[])
     {
         Game game = new Game();
-
-        System.out.println(game.startGame());
+        System.out.println(game.startGame()); 
     }
 
 }
